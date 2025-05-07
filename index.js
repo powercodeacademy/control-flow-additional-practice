97e68e175962034c383a8606051b2d5d248f657c
Mike <mclancy96@gmail.com>
2025-05-07 16:20:08 -0400

    Implement all three because I forgot to commit incrementally

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function lateFee() {
-  // Write your code here!
+function lateFee(daysLate) {
+  if (daysLate <= 0) {
+    return "No fine!"
+  } else if (daysLate <= 7) {
+    return `Your fine is \$${daysLate}.`
+  } else if (daysLate <= 30) {
+    return `Your fine is \$${daysLate * 2}.`
+  }
+  return "Membership suspended."
 }
 
-function branchHours() {
-  // Write your code here!
+function branchHours(branchName) {
+  return branchName == "Central" ? "Open until 9pm." : "Open until 6pm."
 }
 
-function recommendBookByGenre() {
-  // Write your code here!
+function recommendBookByGenre(genre) {
+  switch (genre) {
+    case "fiction":
+      return "Check out our bestsellers section."
+    case 'non-fiction':
+      return 'Explore the top biographies shelf.'
+    case "comics":
+      return 'Visit the graphic novels area.'
+    default:
+      return 'Browse whatever catches your eye!'
+  }
 }
