481d485410fc442685dc733b014a03e831c63a8f
Emily Kanarek <emilyakanarek@gmail.com>
2025-05-08 09:58:14 -0400

    Calculate late fees and penalties

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function lateFee() {
-  // Write your code here!
+function lateFee(daysLate) {
+  let charge = 0;
+  if (daysLate <= 0) {
+    return "No charge!"
+  } else if (daysLate <=7) {
+    charge = daysLate;
+    return `You owe $${charge}.00.`;
+  } else if (daysLate > 7 && daysLate <= 30) {
+    charge = daysLate * 2 - 7;
+    return `You owe $${charge}.00.`;
+  } else {
+    charge = 60;
+    return `You owe $${charge} and your borrowing privileges have been suspended.`
+  }
 }
 
 function branchHours() {
